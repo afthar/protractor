c8949bd6f7a99f292968e21031692884849b5e3f
Daniel Alter <daniel.alter@vendhq.com>
2015-07-28 15:23:05 +1200

    initial commit

---
+++ conf.js
@@ -0,0 +1,15 @@
+
+// conf.js
+exports.config = {
+  seleniumAddress: 'http://192.168.59.103:3000/wd/hub',
+  specs: ['spec.js'],
+  capabilities: {
+      'browserName': 'chrome',
+      shardTestFiles: true,
+      maxInstances: 1,
+        'chromeOptions': {'args': [
+            'proxy-server=192.168.59.103:8090'
+        ]}
+    },
+}
+
