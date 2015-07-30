c509255fc3981087d6682a8053d516922e35dfa8
Daniel Alter <daniel.alter@vendhq.com>
2015-07-30 18:42:47 +1200

    fixed proxy

--- conf.js
+++ conf.js
@@ -1,15 +1,15 @@
 
 // conf.js
 exports.config = {
-  seleniumAddress: 'http://192.168.59.103:3000/wd/hub',
+//  seleniumAddress: 'http://192.168.59.103:3000/wd/hub',
   specs: ['spec.js'],
   capabilities: {
       'browserName': 'chrome',
       shardTestFiles: true,
       maxInstances: 1,
-        'chromeOptions': {'args': [
-            'proxy-server=192.168.59.103:8090'
-        ]}
+//        'chromeOptions': {'args': [
+//            'proxy-server=192.168.59.103:8090'
+//        ]}
     },
 }
 
